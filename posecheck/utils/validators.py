fed57f84fb7c280e2701b32448ac4141a87d5c28
Alex Morehead <alex.morehead@gmail.com>
2024-12-15 15:05:02 -0600

    Fix expected returncode in `is_reduce_installed()`

--- posecheck/utils/validators.py
+++ posecheck/utils/validators.py
@@ -15,7 +15,7 @@ def is_reduce_installed(reduce_path: str = REDUCE_PATH) -> bool:
     """
     try:
         result = subprocess.run([reduce_path, '-version'], capture_output=True, text=True)
-        return result.returncode == 1
+        return result.returncode == 0
     except Exception as e:
         print(f"An error occurred while checking 'reduce': {e}")
         return False
